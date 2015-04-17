539e36d931891bf5f34744918bf8d0d3cf7f5f87
Jonathan Kylstra <jskylstr@cs.unc.edu>
2015-04-17 09:51:45 -0400

    Added Python script to look through all of the files and create the ScanDataAtLandmarks and Errors files. If the final output for a scan is not produced then the script presumes there was an error and checks for the input files needed by the pipeline steps. This lets us know where the error occurred or whether there was simply no segmentation file.
    Errors and ScanData are the files produced by CheckOutputs.py

---
+++ CheckOutputs.py
@@ -0,0 +1,49 @@
+import sys
+import os
+
+def FileExists(folderpath, errorFile, ScanID, FileSuffix):
+	inputFile = os.path.join(folderpath, str(ScanID) + FileSuffix)
+	if os.path.isfile(inputFile):
+		errorFile.write(", Yes ")
+		return
+	errorFile.write(", No ")
+
+
+def main():
+	
+	if len(sys.argv) < 4:
+		print "correct usage: python CheckOutputs.Py <startIndex> <EndIndex> <DatabaseDirectory>"
+		return
+	try:
+		start = int(sys.argv[1])
+		end = int(sys.argv[2])
+	except Exception, e:
+		raise e
+
+	output = open("ScanDataAtLandmarks.csv", 'w')
+	output.write("ScanID, TVC Cross-sectional Area, TVC Perimeter, TVC COM , Subglottic Cross-sectional Area, Subglottic Perimeter, Subglottic COM , InferriorSubglottis Cross-sectional Area, InferriorSubglottis Perimeter, InferriorSubglottis COM, MinSliceID, MinSlice Cross-sectional Area, MinSlice Perimeter, MinSlice COM, MidTrachea Cross-sectional Area, MidTrachea Perimeter, MidTrachea COM\n")
+	errors = open("Errors.csv", 'w')
+	errors.write("ScanID, has _OUTPUT.nrrd, has _LANDMARKS.txt, has _CLIPPINGS.txt, has _OUTPUT.vtp \n")
+
+	for ScanID in xrange(start, end+1):
+
+
+		f = os.path.join(sys.argv[3], str(ScanID), str(ScanID) + "_CROSS_SECTIONS_AT_LANDMARKS.csv")
+		if os.path.isfile(f):
+			o = open(f, 'r')
+			for line in o:
+				output.write(line)
+			continue
+
+		folder = os.path.join(sys.argv[3], str(ScanID))
+		errors.write("%d " % (ScanID))
+		FileExists(folder, errors, ScanID, "_OUTPUT.nrrd")
+		FileExists(folder, errors, ScanID, "_LANDMARKS.txt")
+		FileExists(folder, errors, ScanID, "_CLIPPINGS.txt")
+		FileExists(folder, errors, ScanID, "_OUTPUT.vtp")
+		errors.write("\n")
+
+	# print total, count
+##---------------------------------------------------
+if __name__ == '__main__':
+	main()
